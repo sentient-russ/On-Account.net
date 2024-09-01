8711bdc92661fb1e85fec60c922c9f26b8307b16
Russell Geary <russell@sentientventure.net>
2024-09-01 16:50:07 -0400

    Added admin controller and landing page for role based authorization management. This lacks roles being configured in the program.cs

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace OnAccount.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ILogger<AdminController> logger;
+
+        public AdminController(ILogger<AdminController> logger)
+        {
+            this.logger = logger;
+        }
+        
+        [Authorize(Roles = "Manager")]
+        public IActionResult Index()
+        {
+            ViewData["welcome"] = "Welcome to the admin pannel!";
+            
+            return View();
+        }
+    }
+}
