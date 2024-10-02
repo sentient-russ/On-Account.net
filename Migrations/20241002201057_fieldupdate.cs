ce8b7ffa3c4f46c92d7c388335e92841778d09a6
Russell Geary <russell@sentientventure.net>
2024-10-02 17:43:59 -0400

    more debugging user role change

---
+++ Migrations/20241002201057_fieldupdate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace oa.Migrations
+{
+    public partial class fieldupdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "account_creation_date",
+                schema: "Identity",
+                table: "account",
+                type: "datetime(6)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "account_creation_date",
+                schema: "Identity",
+                table: "account",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)");
+        }
+    }
+}
