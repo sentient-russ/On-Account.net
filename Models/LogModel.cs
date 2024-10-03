459af2c8555504bc2e9476fca1930fa02dcbaf98
Russell Steele <steele.russell@hotmail.com>
2024-10-02 21:55:04 -0400

    sudo code in DbConnector service for my man.

---
+++ Models/LogModel.cs
@@ -0,0 +1,11 @@
+﻿namespace oa.Models
+{
+    public class LogModel
+    {
+        public int Id { get; set; }
+        public DateTime ChangeDate { get; set; }
+        public string UserId { get; set; }
+        public string ChangedFrom { get; set; }
+        public string ChangedTo { get; set; }
+    }
+}
