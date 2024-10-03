9d6cd721ad7bd5fd01fbda527bf6e83e2e0ebd59
Russell Geary <russell@sentientventure.net>
2024-10-02 22:02:53 -0400

    Post database migration for the new log table

--- Areas/Identity/Data/ApplicationDbContext.cs
+++ Areas/Identity/Data/ApplicationDbContext.cs
@@ -17,6 +17,7 @@ public class ApplicationDbContext : IdentityDbContext<AppUser>
     public virtual DbSet<oa.Models.NormalSideModel> account_normal_side_options { get; set; } = default!;
     public virtual DbSet<oa.Models.PassHashModel> pass_hash { get; set; } = default!;
     public virtual DbSet<oa.Models.TransactionModel> transaction { get; set; } = default!;
+    public virtual DbSet<oa.Models.LogModel> log { get; set; } = default!;
 
     //the next section overrides the default db naming
     protected override void OnModelCreating(ModelBuilder builder)
---
+++ Migrations/20241003020038_log-table.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace oa.Migrations
+{
+    public partial class logtable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "log",
+                schema: "Identity",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ChangeDate = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    UserId = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    ChangedFrom = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    ChangedTo = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_log", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "log",
+                schema: "Identity");
+        }
+    }
+}
