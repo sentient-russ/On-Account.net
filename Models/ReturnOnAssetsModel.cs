2b40a98417027f0b03da125e36f39b28bc210526
wawelgreg <141285799+wawelgreg@users.noreply.github.com>
2024-11-17 18:55:30 -0500

    Create ReturnOnAssetsModel.cs

---
+++ Models/ReturnOnAssetsModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc;
+using oa.Services;
+using System.ComponentModel;
+
+namespace oa.Models
+{
+    public class ReturnOnAssetsModel
+    {
+        [ValidateNever]
+        [ModelBinder(BinderType = typeof(CurrencyModelBinder))]
+        [DisplayName("Revenue Balance:")]
+        public decimal? revenues_balance { get; set; }
+
+        [ValidateNever]
+        [ModelBinder(BinderType = typeof(CurrencyModelBinder))]
+        [DisplayName("Expenses Balance:")]
+        public decimal? expenses_balance { get; set; }
+
+        [ValidateNever]
+        [ModelBinder(BinderType = typeof(CurrencyModelBinder))]
+        [DisplayName("Current Assets Balance:")]
+        public decimal? assets_balance { get; set; }
+    }
+}
