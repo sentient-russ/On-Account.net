db2674198fd49b306a487528387978c395b4f280
wawelgreg <141285799+wawelgreg@users.noreply.github.com>
2024-11-17 18:56:56 -0500

    Introduce returnOnAssetsModel to DashboardBundleModel

--- Models/DashboardBundleModel.cs
+++ Models/DashboardBundleModel.cs
@@ -3,10 +3,12 @@
     public class DashboardBundleModel
     {
         public CurrentRaitoModel currentRaitoModel { get; set; }
+        public ReturnOnAssetsModel returnOnAssetsModel { get; set; }
 
-        public DashboardBundleModel(CurrentRaitoModel currentRaitoModel)
+        public DashboardBundleModel(CurrentRaitoModel currentRaitoModel, ReturnOnAssetsModel returnOnAssetsModel)
         {
             this.currentRaitoModel = currentRaitoModel;
+            this.returnOnAssetsModel = returnOnAssetsModel;
         }
     }
 }
