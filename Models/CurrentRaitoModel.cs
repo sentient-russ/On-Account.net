51e7bddf8e746331e5f149acbdb3e53c602a5926
wawelgreg <141285799+wawelgreg@users.noreply.github.com>
2024-11-16 22:02:32 -0500

    Create CurrentRaitoModel.cs

---
+++ Models/CurrentRaitoModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc;
+using oa.Services;
+using System.ComponentModel;
+
+namespace oa.Models
+{
+    public class CurrentRaitoModel
+    {
+        [ValidateNever]
+        [ModelBinder(BinderType = typeof(CurrencyModelBinder))]
+        [DisplayName("Current Assets Balance:")]
+        public decimal? current_assets_balance { get; set; }
+
+        [ValidateNever]
+        [ModelBinder(BinderType = typeof(CurrencyModelBinder))]
+        [DisplayName("Current Liabilities Balance:")]
+        public decimal? current_liabilities_balance { get; set; }
+    }
+}
