61f64fc073ac3db4fe1fc94e99581f9e333b24a5
wawelgreg <141285799+wawelgreg@users.noreply.github.com>
2024-11-17 19:29:41 -0500

    Update DashboardBundleModel.cs

--- Models/DashboardBundleModel.cs
+++ Models/DashboardBundleModel.cs
@@ -4,11 +4,17 @@
     {
         public CurrentRaitoModel currentRaitoModel { get; set; }
         public ReturnOnAssetsModel returnOnAssetsModel { get; set; }
+        public ReturnOnEquityModel returnOnEquityModel { get; set; }
 
-        public DashboardBundleModel(CurrentRaitoModel currentRaitoModel, ReturnOnAssetsModel returnOnAssetsModel)
+        public DashboardBundleModel(
+            CurrentRaitoModel currentRaitoModel, 
+            ReturnOnAssetsModel returnOnAssetsModel, 
+            ReturnOnEquityModel returnOnEquityModel
+            )
         {
             this.currentRaitoModel = currentRaitoModel;
             this.returnOnAssetsModel = returnOnAssetsModel;
+            this.returnOnEquityModel = returnOnEquityModel;
         }
     }
 }
