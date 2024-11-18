ca5a34a5ea23205198fefee05337fdb6d54f95b4
wawelgreg <141285799+wawelgreg@users.noreply.github.com>
2024-11-17 21:13:53 -0500

    Create QuickRatioModel.cs

---
+++ Models/QuickRatioModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc;
+using oa.Services;
+using System.ComponentModel;
+
+namespace oa.Models
+{
+    public class QuickRatioModel
+    {
+        [ValidateNever]
+        [ModelBinder(BinderType = typeof(CurrencyModelBinder))]
+        [DisplayName("Current Assets Balance:")]
+        public decimal? current_assets_balance { get; set; }
+
+        [ValidateNever]
+        [ModelBinder(BinderType = typeof(CurrencyModelBinder))]
+        [DisplayName("Current Liabilities Balance:")]
+        public decimal? current_liabilities_balance { get; set; }
+
+        [ValidateNever]
+        [ModelBinder(BinderType = typeof(CurrencyModelBinder))]
+        [DisplayName("Inventory Balance:")]
+        public decimal? inventory_balance { get; set; }
+    }
+}
