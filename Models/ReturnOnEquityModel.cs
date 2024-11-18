c7cc64d806f1f11c45c36dd1a34442425f4ea800
wawelgreg <141285799+wawelgreg@users.noreply.github.com>
2024-11-17 19:28:51 -0500

    Create ReturnOnEquityModel.cs

---
+++ Models/ReturnOnEquityModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc;
+using oa.Services;
+using System.ComponentModel;
+
+namespace oa.Models
+{
+    public class ReturnOnEquityModel
+    {
+        [ValidateNever]
+        [ModelBinder(BinderType = typeof(CurrencyModelBinder))]
+        [DisplayName("Revenue Balance:")]
+        public decimal? revenues_balance { get; set; }
+
+        [ValidateNever]
+        [ModelBinder(BinderType = typeof(CurrencyModelBinder))]
+        [DisplayName("Expenses Balance:")]
+        public decimal? expenses_balance { get; set; }
+
+        [ValidateNever]
+        [ModelBinder(BinderType = typeof(CurrencyModelBinder))]
+        [DisplayName("Equity Balance:")]
+        public decimal? equity_balance { get; set; }
+    }
+}
