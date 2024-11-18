7f3a6c9a1b339a79772162705cfb4a6f774d0643
wawelgreg <141285799+wawelgreg@users.noreply.github.com>
2024-11-17 21:14:30 -0500

    Add quick ratio model

--- Models/DashboardBundleModel.cs
+++ Models/DashboardBundleModel.cs
@@ -5,16 +5,19 @@
         public CurrentRaitoModel currentRaitoModel { get; set; }
         public ReturnOnAssetsModel returnOnAssetsModel { get; set; }
         public ReturnOnEquityModel returnOnEquityModel { get; set; }
+        public QuickRatioModel quickRatioModel { get; set; }
 
         public DashboardBundleModel(
-            CurrentRaitoModel currentRaitoModel, 
-            ReturnOnAssetsModel returnOnAssetsModel, 
-            ReturnOnEquityModel returnOnEquityModel
+            CurrentRaitoModel currentRaitoModel,
+            ReturnOnAssetsModel returnOnAssetsModel,
+            ReturnOnEquityModel returnOnEquityModel,
+            QuickRatioModel quickRatioModel
             )
         {
             this.currentRaitoModel = currentRaitoModel;
             this.returnOnAssetsModel = returnOnAssetsModel;
             this.returnOnEquityModel = returnOnEquityModel;
+            this.quickRatioModel = quickRatioModel;
         }
     }
 }
